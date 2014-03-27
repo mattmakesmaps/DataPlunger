2f044a531b0119357db8843abdf8eab907d3065b
Matthew Kenny <matthewkenny@gmail.com>
2014-03-26 18:37:07 -0700

    added additional processor tests

--- dataplunger/tests/tests_processors.py
+++ dataplunger/tests/tests_processors.py
@@ -1,20 +1,94 @@
 __author__ = 'matt'
 __date__ = '3/2/14'
 from dataplunger.processors import *
+import os
+import tempfile
 from nose.tools import raises
 
 
 class RecordConstructorMock(object):
-    def __init__(self, records):
-        self.records = records
+    def __init__(self, **kwargs):
+        self.records = []
 
 
 class TestBase(object):
     def __init__(self):
         self.records = [{'name': 'Matt', 'age': '27', 'gender': 'male'}]
-        self.record_constructor = RecordConstructorMock(self.records)
+        self.record_constructor = RecordConstructorMock(records=self.records)
         self.devnull = ProcessorDevNull(self.record_constructor)
 
+class TestProcessorCSVWriter(TestBase):
+    """
+    CSV writer should output a set of input records as a CSV file.
+    """
+
+    def setup(self):
+        """
+        Create a test file to store CSV output.
+        """
+        self.test_file = tempfile.mkstemp()
+
+    def test_csvwriter(self):
+        """
+        Write records as CSV to a temporary output file.
+        CSV should match expected values.
+        """
+        csv_writer = ProcessorCSVWriter(self.devnull, self.test_file[1], ['name', 'age', 'gender'])
+        csv_writer.process(self.records)
+        # Check the output file.
+        # test_file returns a tuple, with the path as the second element.
+        with open(self.test_file[1], 'r') as test_file_handle:
+            contents = test_file_handle.readline().strip()
+            assert contents == 'Matt,27,male'
+
+    def teardown(self):
+        """
+        Delete temp file if it still exists.
+        """
+        if os.path.isfile(self.test_file[1]):
+            os.remove(self.test_file[1])
+
+
+class TestProcessorDevNull(object):
+    """
+    ProcessorDevNull should populate its record_constructor's records attribute
+    with the records that were passed into it. This resembles the end result of
+    a processing chain's execution.
+    """
+    def __init__(self):
+        self.records = [{'name': 'Matt', 'age': '27', 'gender': 'male'}]
+        self.records_constructor = RecordConstructorMock()
+
+    def test_devnull(self):
+        devnull = ProcessorDevNull(self.records_constructor)
+        devnull.process(self.records)
+        assert self.records_constructor.records == self.records
+
+
+class TestProcessorSortRecords(object):
+    """
+    Test ProcessorSortRecords. Currently, only an ascending sort
+    on strings is implemented.
+    """
+    def __init__(self):
+        self.records = [{'name': 'Matt', 'age': '27', 'gender': 'male'},
+                        {'name': 'Bob', 'age': '30', 'gender': 'male'},
+                        {'name': 'Luke', 'age': '31', 'gender': 'male'}]
+        self.record_constructor = RecordConstructorMock(records=self.records)
+        self.devnull = ProcessorDevNull(self.record_constructor)
+
+    def test_sortrecords(self):
+        """
+        Sort based on name field. Should return records in the order of:
+        'Bob', 'Luke', 'Matt'
+        """
+        expected_list = [
+            {'name': 'Bob', 'age': '30', 'gender': 'male'},
+            {'name': 'Luke', 'age': '31', 'gender': 'male'},
+            {'name': 'Matt', 'age': '27', 'gender': 'male'}
+        ]
+        sort_processor = ProcessorSortRecords(self.devnull, sortby='name')
+        assert sort_processor.process(self.records) == expected_list
 
 class TestProcessorMatchValue(TestBase):
     """
