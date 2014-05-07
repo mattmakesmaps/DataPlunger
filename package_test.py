a4de56787c14ce0537d62ce102529cd6dc50b7c8
Matthew Kenny <matthewkenny@gmail.com>
2014-05-07 15:29:35 -0700

    update of package_test for census data

--- package_test.py
+++ package_test.py
@@ -1,28 +1,16 @@
 __author__ = 'mkenny'
 
 from dataplunger import *
-from pprint import pprint
 from datetime import datetime
 
 if __name__ == '__main__':
     # Build a config object.
     start_time = datetime.now()
-    config_path = '/users/matt/Projects/DataPlunger/sample_configs/multi_reader.json'
+    config_path = '/home/mkenny/Projects/DataPlunger/sample_configs/us_all_census.json'
     collectionConfig = Configuration()
     collectionConfig.parse_config(config_path)
 
-    # solos = Controller(collectionConfig, "USCensusConfigSolos")
-    # combined = Controller(collectionConfig, "USCensusConfigCombined")
-    heap = Controller(collectionConfig, "HeapSortConfig")
-    # multireader = Controller(collectionConfig, "SHPConfig")
-    # multireader = Controller(collectionConfig, "WACensusConfig")
-    # multireader = Controller(collectionConfig, "FreebaseConfig")
-    # solos.process_layers()
-    # solos_time = datetime.now()-start_time
-    # print "Solos Time: %s" % solos_time
-
-    # combined.process_layers()
-    # combined_time = datetime.now()-start_time
-    heap.process_layers()
+    combined = Controller(collectionConfig, "USCensusConfigCombined")
+    combined.process_layers()
     combined_time = datetime.now()-start_time
     print "Combined Time: %s" % combined_time
